ff2849b8c201c2e37034890fb6e43325e43e4157
kuboshiba <r115313@g.kobe-kosen.ac.jp>
2018-09-28 18:20:47 +0900

    最後に改行を追加

--- PrintTxtFile.java
+++ PrintTxtFile.java
@@ -4,6 +4,7 @@ public class PrintTxtFile {
     try (BufferedReader in = new BufferedReader(new FileReader(new File(path)))) {
       String line;
       while((line = in.readLine()) != null) System.out.println(line);
+      System.out.println();
     } catch (FileNotFoundException e) {
       e.printStackTrace();
       System.exit(-1); // 0 以外は異常終了
