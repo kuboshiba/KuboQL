da539fcab59aecdf4e63b5a6ad724b9312e7db81
r115311 <43838592+r115311@users.noreply.github.com>
2018-11-01 13:43:12 +0900

    select fromに対応

--- ControlSelect.java
+++ ControlSelect.java
@@ -1,52 +1,11 @@
 import java.io.*;
 public class ControlSelect {
   Define d = new Define();
-  ControlSelect(String database, String[] cmd, String input) {
+  ControlSelect(String database, String[] cmd) {
     if(cmd.length == 3 && cmd[1].equals("*from")) SelectAll("./databases/" + database + "/" + cmd[2]);
     else if(cmd.length == 4 && cmd[1].equals("*") && cmd[2].equals("from")) SelectAll("./databases/" + database + "/" + cmd[3]);
+  	else if(cmd.length == 4 && cmd[2].equals("from")) new SelectColumn("./databases/" + database + "/" + cmd[3], cmd[1]);			//追加
     else if(cmd.length == 3 && cmd[1].equals("from")) d.error("select コマンドの使い方が間違っています help か \\h で参照してください");
-    else SelectDesignation(database, cmd, input);
-  }
-
-  void SelectDesignation(String database, String[] cmd, String input) {
-    String _temp = input.replace("select","");
-    _temp = _temp.substring(1, _temp.indexOf("from")-1);
-    _temp = _temp.replace(" ", "");
-
-    String[] column_list = _temp.split(",");
-    String path = "./databases/" + database + "/" + cmd[cmd.length-1];
-
-    CheckExistTable checkExistTable = new CheckExistTable();
-    if (checkExistTable.exist(path)) {
-      if (checkExistTable.exist_column(path, column_list)) PrintSelectDesignation(path, column_list);
-      else d.error("指定されたカラムは存在しません desc [テーブル名] で確認してください");
-    }
-    else d.error("そのテーブルは存在しません show tables で確認してください");
-  }
-
-  void PrintSelectDesignation(String path, String[] column_list) {
-    File file = new File(path);
-    try (BufferedReader in = new BufferedReader(new FileReader(new File(path)))) {
-      String line;
-      String[] line_split;
-      int[] culum_len = new int[50];
-      int cnt = 0;
-      while((line = in.readLine()) != null) {
-        line_split = line.split(",");
-        if (line_split.length == column_list.length) {
-          SelectAll(path);
-          break;
-        }
-        if (cnt == 0) {
-        }
-      }
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-      System.exit(-1); // 0 以外は異常終了
-    } catch (IOException e) {
-      e.printStackTrace();
-      System.exit(-1);
-    }
   }
 
   void SelectAll(String path) {
@@ -62,8 +21,8 @@ public class ControlSelect {
       String[] line_split;  // 読み込んだ1行を分割
       int[] culum_len = new int[50];  // カラム数は最大50個まで
 
-      while((line = in.readLine()) != null) {
-        line_split = line.split(",");
+      while((line = in.readLine()) != null) {  //  1行ずつ読み込み
+        line_split = line.split(",");  //  ","区切りで格納
         if(line_split[0].equals("@eof")) {
           System.out.print("+");
           for(i=0; i<n; i++) {
@@ -75,28 +34,28 @@ public class ControlSelect {
           break;
         }
         if (cnt == 0 ) {
-          n = line_split.length;
+          n = line_split.length;  //  文字数格納
           for(i=0; i<line_split.length; i++) culum_len[i] = Integer.parseInt(line_split[i]);
           System.out.print("+");
           for(i=0; i<line_split.length; i++) {
             for(j=0; j<culum_len[i]; j++) System.out.print("-");
             System.out.print("+");
           }
-          System.out.println();
+          System.out.println(); //  ここまで最初の行(線)
         }  else {
           for(i=0; i<line_split.length; i++) {
             System.out.print("| " + line_split[i]);
             int temp = culum_len[i] - line_split[i].length() - 1;
-            for(j=0; j<temp; j++) System.out.print(" ");
+            for(j=0; j<temp; j++) System.out.print(" ");  //  左詰め
           }
-          System.out.println("|");
+          System.out.println("|");  //  ここまで1行出力 
           if(cnt == 1) {
             System.out.print("+");
             for(i=0; i<line_split.length; i++) {
               for(j=0; j<culum_len[i]; j++) System.out.print("-");
               System.out.print("+");
             }
-            System.out.println();
+            System.out.println();  //  ここまで区切り線
           }
         }
         cnt++;
